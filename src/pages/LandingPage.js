e7254b83254be81cebdcd517b196f04b584b7880
Juan <albites.tapia@gmail.com>
2022-05-12 17:39:26 -0500

    first commit

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Aside } from '../components/Aside';
+import { BoxChatMessage } from '../components/BoxChatMessage';
 
 export const LandingPage = () => {
   return (
     <>
-      <Aside />
+      <BoxChatMessage />
     </>
   );
 };
