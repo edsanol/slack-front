a17e102b95242017bd2f672f1228b1643ae0f423
danielfelipeq <danielquispe.m@gmail.com>
2022-05-28 22:36:31 -0500

    feat: update reactions

--- src/components/ReactionChat.js
+++ src/components/ReactionChat.js
@@ -4,11 +4,11 @@ import '../assets/styles/components/ReactionChat.scss';
 export const ReactionChat = () => {
   return (
     <div className="reaction__div-chat">
-      <i className='bx bx-smile' ></i>
-      <i className='bx bx-message-rounded-dots'></i>
-      <i className='bx bx-share'></i>
+      <i className="bx bx-smile"></i>
+      <i className="bx bx-message-rounded-dots"></i>
+      {/* <i className="bx bx-share"></i>
       <i className='bx bx-bookmark' ></i>
-      <i className='bx bx-dots-vertical-rounded' ></i>
+      <i className='bx bx-dots-vertical-rounded' ></i> */}
     </div>
   );
-}
+};
