abd6d93a15d4691a27826bf77372d95fd6278b15
AlejandroAlbites <81944337+AlejandroAlbites@users.noreply.github.com>
2022-06-09 12:15:38 -0500

    feature socket config client

---
+++ src/hooks/useSocket.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+export const useSocket = (serverURL) => {
+  const [socket, setSocket] = useState(null);
+  const [online, setOnline] = useState('');
+  const token = localStorage.getItem('token');
+
+  useEffect(() => {
+    if (!token) {
+      return false;
+    }
+
+    const newSocket = io.connect(serverURL, {
+      transports: ['websocket'],
+      autoConnect: true,
+      forceNew: true,
+      query: {
+        'x-token': token,
+      },
+    });
+
+    setSocket(newSocket);
+    setOnline('enable');
+  }, [serverURL, token]);
+
+  return { socket, online };
+};
